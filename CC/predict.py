79f2717bb1c5c3ed511d4d56d4bcab68650b6168
kdanu <76394482+kdanu@users.noreply.github.com>
2021-06-12 14:02:53 +0700

    Update predict.py

--- CC/predict.py
+++ CC/predict.py
@@ -5,6 +5,7 @@ import numpy as np
 from PIL import Image
 from keras.preprocessing import image
 
+model = tf.keras.models.load_model('model.h5')
 model.compile(
     optimizer='adam',
     loss='categorical_crossentropy',
